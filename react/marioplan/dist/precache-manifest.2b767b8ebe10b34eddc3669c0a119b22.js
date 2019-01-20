96ec5955c4147f29d8546c2f6a9c81b4f97e7c22
Jonathan <jonathanmatheus1997.jm@gmail.com>
2019-01-20 12:53:56 -0300

    [#marioplan] Deploy firebase

---
+++ react/marioplan/dist/precache-manifest.2b767b8ebe10b34eddc3669c0a119b22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "20fa5a2563b2cd7a8d83",
+    "url": "/static/js/main.20fa5a25.chunk.js"
+  },
+  {
+    "revision": "4b76802afdc101663c02",
+    "url": "/static/js/1.4b76802a.chunk.js"
+  },
+  {
+    "revision": "20fa5a2563b2cd7a8d83",
+    "url": "/static/css/main.2f585e5e.chunk.css"
+  },
+  {
+    "revision": "2d0c251305a597fb3be416da5dca2b18",
+    "url": "/index.html"
+  }
+];

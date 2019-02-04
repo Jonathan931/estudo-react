b82b72b986ba965944cf15983759dc5eec7214f3
Jonathan Matheus <jonathanmatheus@Insights-Mac-mini.local>
2019-02-04 11:55:46 -0300

    [#TripPlanner] Criação de stykes para componente Trip.

--- react-native/TripPlanner/src/screens/TripScreen/index.js
+++ react-native/TripPlanner/src/screens/TripScreen/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { View, Text, FlatList, TouchableOpacity, Image } from 'react-native';
+import styles from './styles';
 
 export default class TripScreen extends Component {
 
@@ -13,38 +14,25 @@ export default class TripScreen extends Component {
       price: 'R$ 5000',
       places: [
         {id: '1', name: 'Amsterdan', description: 'Chegada', price: 100, lat: 0, long: 0},
-        {id: '2', name: 'Bruxelas', description: 'Hospedagem', price: 100, lat: 0, long: 0}
+        {id: '2', name: 'Bruxelas', description: 'Hospedagem', price: 100, lat: 0, long: 0},
+        {id: '3', name: 'Amsterdan', description: 'Chegada', price: 100, lat: 0, long: 0},
+        {id: '4', name: 'Bruxelas', description: 'Hospedagem', price: 100, lat: 0, long: 0},
+        {id: '5', name: 'Amsterdan', description: 'Chegada', price: 100, lat: 0, long: 0},
+        {id: '6', name: 'Bruxelas', description: 'Hospedagem', price: 100, lat: 0, long: 0},
+        {id: '7', name: 'Amsterdan', description: 'Chegada', price: 100, lat: 0, long: 0},
+        {id: '8', name: 'Bruxelas', description: 'Hospedagem', price: 100, lat: 0, long: 0},
       ]
     }
     return (
-      <View style={{flex: 1}}>
-        <View style={{
-          height: 192, 
-          backgroundColor: 'grey'}}>
-          <View style={{
-            position: 'absolute',
-            top: 26,
-            left: 16,
-            padding: 10
-          }}>
+      <View style={styles.wrapper}>
+        <View style={styles.header}>
+          <View style={styles.backButton}>
           <TouchableOpacity onPress={()=>{ this.props.navigation.goBack()}}>
             <Image source={require('../../../assets/row-left.png')}/>
           </TouchableOpacity>
         </View>
-          <Text style={{
-            position: 'absolute',
-            left: 16,
-            bottom: 16
-          }}>{trip.name}</Text>
-          <Text style={{
-           position: 'absolute', 
-           bottom: 16, 
-           right: 32, 
-           textAlign: 'right',
-           backgroundColor: '#180079', 
-           padding: 4, 
-           color: 'white'
-          }}>{trip.price}</Text>
+          <Text style={styles.tripName}>{trip.name}</Text>
+          <Text style={styles.tripPrice}>{trip.price}</Text>
         </View>
         <FlatList
           style={{flex: 1}} 
@@ -60,17 +48,14 @@ export default class TripScreen extends Component {
   }
   renderItem = ({item}) =>{
     return(
-      <View style={{flex: 1}}>
-
-        <View style={{flex: 1}}>
-          <Text>{item.name}</Text>
+      <View style={styles.item}>
+        <View style={styles.itemInfo}>
+          <Text style={styles.itemName}>{item.name}</Text>
           <Text>{item.description}</Text>
         </View>
-
-        <View style={{textAlign: 'center'}}>
-          <Text>{item.price}</Text>
+        <View style={styles.wrapperItemPrice}>
+          <Text style={styles.itemPrice}>{item.price}</Text>
         </View>  
-        
       </View>
     )
   }
---
+++ react-native/TripPlanner/src/screens/TripScreen/styles.js
@@ -0,0 +1,55 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  wrapper:{
+    flex: 1,
+  },
+  header:{ 
+    height: 192, 
+    backgroundColor: 'grey'
+  },
+  backButton:{
+    position: 'absolute',
+    top: 26,
+    left: 16,
+    padding: 10
+  },
+  tripName:{
+    position: 'absolute',
+    left: 16,
+    bottom: 16
+  },
+  tripPrice:{
+    position: 'absolute', 
+    bottom: 16, 
+    right: 32, 
+    textAlign: 'right',
+    backgroundColor: '#180079', 
+    padding: 4, 
+    color: 'white'
+  },
+  item:{
+    flex: 1,
+    flexDirection: 'row',
+    paddingBottom: 16
+  },
+  itemName:{
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  wrapperItemPrice:{ 
+    alignItems: 'center', 
+    justifyContent: 'center', 
+    paddingRight: 16
+  },
+  itemPrice:{
+    textAlign: 'right', 
+    color: '#24C6DC', 
+    fontWeight: 'bold'
+  },
+  itemInfo: {
+    flex: 1
+  }
+})
+
+export default styles;

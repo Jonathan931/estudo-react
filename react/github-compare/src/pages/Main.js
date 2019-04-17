173044439bfc7a1d5f3db74d9481784387be37f8
Jonathan Matheus <jonathanmatheus@Insights-Mac-mini.local>
2019-04-17 10:58:58 -0300

    [#github-compare] Adicionando styled em input e button.

--- react/github-compare/src/pages/Main.js
+++ react/github-compare/src/pages/Main.js
@@ -1,10 +1,51 @@
 import React from 'react';
+import styled from 'styled-components';
 import logo from '../assests/logo.png';
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 60px;
+`;
+
+const Form = styled.form`
+  margin-top: 20px;
+  width: 100%;
+  max-width: 400px;
+  display: flex;
+
+  input {
+    flex: 1;
+    height: 55px;
+    padding: 0 20px;
+    background: #fff;
+    border: 0;
+    font-size: 18px;
+    color: #444;
+    border-radius: 3px;
+  }
+
+  button {
+    height: 55px;
+    padding: 0 20px;
+    margin-left: 10px;
+    background: #63f5b6;
+    color: #fff;
+    font-size: 20px;
+    font-weight: bold;
+    border-radius: 3px;
+  }
+`;
+
 const Main = () => (
-  <div>
+  <Container>
     <img src={logo} alt="Github Compare" />
-  </div>
+    <Form>
+      <input type="text" placeholder="usuário/repositório" />
+      <button type="submit">OK</button>
+    </Form>
+  </Container>
 );
 
 export default Main;

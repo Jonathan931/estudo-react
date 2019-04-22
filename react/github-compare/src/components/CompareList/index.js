fd92cfc01ea0b4b5773a441d97effc7093c9a3ac
Jonathan Matheus <jonathanmatheus@Insights-Mac-mini.local>
2019-04-22 11:41:11 -0300

    [#github-compare] Importando axios e fazendo requisição na api do github.

--- react/github-compare/src/components/CompareList/index.js
+++ react/github-compare/src/components/CompareList/index.js
@@ -1,99 +1,46 @@
 import React from 'react';
+import Proptypes from 'prop-types';
 import { Container, Repository } from './styles';
 
-const CompareList = () => (
+const CompareList = ({ repositories }) => (
   <Container>
-    <Repository>
-      <header>
-        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="facebook" />
-        <strong>react</strong>
-        <small>facebook</small>
-        <ul>
-          <li>
-            95,019
+    {repositories.map(repository => (
+      <Repository>
+        <header>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <strong>{repository.name}</strong>
+          <small>{repository.owner.login}</small>
+          <ul>
+            <li>
+              {repository.stargazers_count}
+              {' '}
+              <small>starts</small>
+            </li>
+            <li>
+              {repository.forks_count}
+              {' '}
+              <small>forks</small>
+            </li>
+            <li>
+              {repository.open_issues_count}
+              {' '}
+              <small>issues</small>
+            </li>
             {' '}
-            <small>starts</small>
-          </li>
-          <li>
-            95,019
-            {' '}
-            <small>forks</small>
-          </li>
-          <li>
-            95,019
-            {' '}
-            <small>issues</small>
-          </li>
-          {' '}
-          <li>
-            3 days ago
-            {' '}
-            <small>last commit</small>
-          </li>
-        </ul>
-      </header>
-    </Repository>
-    <Repository>
-      <header>
-        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="facebook" />
-        <strong>react</strong>
-        <small>facebook</small>
-        <ul>
-          <li>
-            95,019
-            {' '}
-            <small>starts</small>
-          </li>
-          <li>
-            95,019
-            {' '}
-            <small>forks</small>
-          </li>
-          <li>
-            95,019
-            {' '}
-            <small>issues</small>
-          </li>
-          {' '}
-          <li>
-            3 days ago
-            {' '}
-            <small>last commit</small>
-          </li>
-        </ul>
-      </header>
-    </Repository>
-    <Repository>
-      <header>
-        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="facebook" />
-        <strong>react</strong>
-        <small>facebook</small>
-        <ul>
-          <li>
-            95,019
-            {' '}
-            <small>starts</small>
-          </li>
-          <li>
-            95,019
-            {' '}
-            <small>forks</small>
-          </li>
-          <li>
-            95,019
-            {' '}
-            <small>issues</small>
-          </li>
-          {' '}
-          <li>
-            3 days ago
-            {' '}
-            <small>last commit</small>
-          </li>
-        </ul>
-      </header>
-    </Repository>
+            <li>
+              {repository.pushed_at}
+              {' '}
+              <small>last commit</small>
+            </li>
+          </ul>
+        </header>
+      </Repository>
+    ))}
   </Container>
 );
+
+CompareList.prototype = {
+  // repositories: Proptypes.arrayOf(Proptypes.shape)
+};
 
 export default CompareList;
--- react/github-compare/src/pages/Main/index.js
+++ react/github-compare/src/pages/Main/index.js
@@ -1,18 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Container, Form } from './styles';
 import CompareList from '../../components/CompareList';
+import api from '../../services/api';
 import logo from '../../assests/logo.png';
 
-const Main = () => (
-  <Container>
-    <img src={logo} alt="Github Compare" />
-    <Form>
-      <input type="text" placeholder="usuário/repositório" />
-      <button type="submit">OK</button>
-    </Form>
+export default class Main extends Component {
+  state = {
+    repositoryInput: '',
+    repositories: [],
+  };
 
-    <CompareList />
-  </Container>
-);
+  render() {
+    const { repositories, repositoryInput } = this.state;
 
-export default Main;
+    return (
+      <Container>
+        <img src={logo} alt="Github Compare" />
+        <Form onSubmit={this.handleAddRepository}>
+          <input
+            type="text"
+            placeholder="usuário/repositório"
+            value={repositoryInput}
+            onChange={this.handleChangeName}
+          />
+          <button type="submit">OK</button>
+        </Form>
+
+        <CompareList repositories={repositories} />
+      </Container>
+    );
+  }
+
+  handleChangeName = (e) => {
+    this.setState({ repositoryInput: e.target.value });
+  };
+
+  handleAddRepository = async (e) => {
+    e.preventDefault();
+    try {
+      const { repositories, repositoryInput } = this.state;
+      const response = await api.get(`/repos/${repositoryInput}`);
+
+      this.setState({
+        repositoryInput: '',
+        repositories: [...repositories, response.data],
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+}

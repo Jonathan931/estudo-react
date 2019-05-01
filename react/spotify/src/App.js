fabb25df5d3165934cc27e3ce45b77fa4ba680dc
Jonathan <jonathanmatheus1997.jm@gmail.com>
2019-05-01 15:45:53 -0300

    [#app-spotify] Criando prototipo da sidebar.

--- react/spotify/src/App.js
+++ react/spotify/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Sidebar from './components/Sidebar';
 import { GlobalStyles } from './styles/global';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Teste</h1>
+    <div style={{ width: '100%', height: '100%' }} className="App">
+      <Sidebar />
       <GlobalStyles />
     </div>
   );
--- react/spotify/src/components/Sidebar/index.js
+++ react/spotify/src/components/Sidebar/index.js
@@ -1,9 +1,61 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Container, NewPlaylist, Nav } from './styles';
+import AddPlaylistIcon from '../../assets/images/add_playlist.svg';
 
-// import { Container } from './styles';
+const Sidebar = () => (
+  <Container>
+    <div>
+      <Nav main>
+        <li>
+          <a href="">Navegar</a>
+        </li>
+        <li>
+          <a href="">Rádio</a>
+        </li>
+      </Nav>
+      <Nav>
+        <li>
+          <span>SUA BIBLIOTECA</span>
+        </li>
+        <li>
+          <a href="">Seu Daily Mix</a>
+        </li>
+        <li>
+          <a href="">Tocados recentemente</a>
+        </li>
+        <li>
+          <a href="">Músicas</a>
+        </li>
+        <li>
+          <a href="">Álbums</a>
+        </li>
+        <li>
+          <a href="">Estações</a>
+        </li>
+        <li>
+          <a href="">Arquivos locais</a>
+        </li>
+        <li>
+          <a href="">Vídeos</a>
+        </li>
+        <li>
+          <a href="">Podcasts</a>
+        </li>
+      </Nav>
+      <Nav>
+        <li>
+          <span>Playlists</span>
+        </li>
+        <li>
+          <a href="">Melhores do rock</a>
+        </li>
+      </Nav>
+    </div>
+    <NewPlaylist>
+      <img src={AddPlaylistIcon} alt="Adicionar Playlist" />
+      Nova playlist
+    </NewPlaylist>
+  </Container>
+);
 
-export default class Sidebar extends Component {
-  render() {
-    return <div />;
-  }
-}
+export default Sidebar;

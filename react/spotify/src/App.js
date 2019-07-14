79472af2ed33517c98a8dd2b87ebaf03b200ee10
Jonathan Matheus <jonathan@MacBook-de-Jonathan.local>
2019-07-14 17:20:21 -0300

    [#app-spotity] Adicionando JSON server e react redux saga e ducks.

--- react/spotify/src/App.js
+++ react/spotify/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import './config/reactotron';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Player from './components/Player';
@@ -8,22 +10,25 @@ import { Wrapper, Container, Content } from './styles/components';
 import { GlobalStyles } from './styles/global';
 
 import Routes from './routes';
+import store from './store';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Wrapper>
-        <Container>
-          <Sidebar />
-          <Content>
-            <Header />
-            <Routes />
-          </Content>
-        </Container>
-        <Player />
-        <GlobalStyles />
-      </Wrapper>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Wrapper>
+          <Container>
+            <Sidebar />
+            <Content>
+              <Header />
+              <Routes />
+            </Content>
+          </Container>
+          <Player />
+          <GlobalStyles />
+        </Wrapper>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
---
+++ react/spotify/src/store/index.js
@@ -0,0 +1,27 @@
+import { createStore, compose, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+
+import reducers from './ducks';
+import sagas from './sagas';
+
+const middlewares = [];
+
+const sagaMonitor = process.env.NODE_ENV === 'development' ? console.tron.createSagaMonitor() : null;
+
+const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
+
+middlewares.push(sagaMiddleware);
+
+const tronMiddleware = process.env.NODE_ENV === 'development' ? console.tron.createEnhancer : () => {};
+
+const store = createStore(
+  reducers,
+  compose(
+    applyMiddleware(...middlewares),
+    tronMiddleware(),
+  ),
+);
+
+sagaMiddleware.run(sagas);
+
+export default store;
